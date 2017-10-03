2829b0ff8592bef70a9d03ff153c8fd169a167f0
Edwin <edwincruz25@unitec.edu>
2017-10-02 21:09:27 -0600

    refactored With ES6

--- scripts.js
+++ scripts.js
@@ -5,11 +5,11 @@ const itemsURL =
   'http://ddragon.leagueoflegends.com/cdn/7.19.1/data/en_US/item.json';
 const itemImageURL = 'http://ddragon.leagueoflegends.com/cdn/7.19.1/img/item/';
 const levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-var count = 0;
-var items = [];
-var itemsChampion1 = [];
-var itemsChampion2 = [];
-var statsCurrentValuesChampion1 = {
+let count = 0;
+let items = [];
+let itemsChampion1 = [];
+let itemsChampion2 = [];
+let statsCurrentValuesChampion1 = {
   hp: 0,
   mp: 0,
   armor: 0,
@@ -20,7 +20,7 @@ var statsCurrentValuesChampion1 = {
   attackspeed: 0,
   critchance: 0
 };
-var statsCurrentValuesChampion2 = {
+let statsCurrentValuesChampion2 = {
   hp: 0,
   mp: 0,
   armor: 0,
@@ -32,7 +32,7 @@ var statsCurrentValuesChampion2 = {
   critchance: 0
 };
 
-var statsInformation = [
+const statsInformation = [
   { hp: ['FlatHPPoolMod', 'rFlatHPModPerLevel', 'PercentHPPoolMod'] },
   { mp: ['FlatMPPoolMod', 'rFlatMPModPerLevel', 'PercentMPPoolMod'] },
   { armor: ['FlatArmorMod', 'rFlatArmorModPerLevel', 'PercentArmorMod'] },
@@ -75,76 +75,76 @@ var statsInformation = [
 
 loadInitialData();
 
-$('.championSelect').change(function() {
+$('.championSelect').change(() => {
   $.getJSON(URL)
-    .done(function(data) {
+    .done(data => {
       updatePictures(data);
       updateStats(data);
     })
-    .fail(function(error) {
+    .fail(error => {
       console.log('Error');
     });
   $.getJSON(itemsURL)
-    .done(function(data) {
+    .done(data => {
       updateItemStats(data);
     })
-    .fail(function(error) {
+    .fail(error => {
       console.log(error);
     });
 });
 
-$('.itemSelect').change(function() {
+$('.itemSelect').change(() => {
   $.getJSON(itemsURL)
-    .done(function(data) {
+    .done(data => {
       updateItemStats(data);
     })
-    .fail(function(error) {
+    .fail(error => {
       console.log('Error');
     });
 });
 
-$('.levelSelect').change(function() {
+$('.levelSelect').change(() => {
   $.getJSON(URL)
-    .done(function(data) {
+    .done(data => {
       updateStats(data);
     })
-    .fail(function(error) {
+    .fail(error => {
       console.log('Error');
     });
   $.getJSON(itemsURL)
-    .done(function(data) {
+    .done(data => {
       updateItemStats(data);
     })
-    .fail(function(error) {
+    .fail(error => {
       console.log('Error');
     });
 });
 
 function loadInitialData() {
   $.getJSON(URL)
-    .done(function(data) {
+    .done(data => {
       updateChampionSelects(data);
       updatePictures(data);
       updateStats(data);
     })
-    .fail(function(error) {
+    .fail(error => {
       console.log('Error');
     });
   $.getJSON(itemsURL)
-    .done(function(data) {
+    .done(data => {
       updateItemSelects(data);
     })
-    .fail(function(error) {
+    .fail(error => {
       console.log('Error');
     });
   $.getJSON(itemsURL)
-    .done(function(data) {
+    .done(data => {
       loadImages(data);
     })
-    .fail(function(error) {
+    .fail(error => {
       console.log('Error');
     });
-  $.each(levels, function(l, level) {
+  $.each(levels, (l, level) => {
     $('.levelSelect').append(
       $('<option>', {
         value: level,
@@ -155,7 +155,7 @@ function loadInitialData() {
 }
 
 function updateChampionSelects(data) {
-  $.each(data.data, function(c, champion) {
+  $.each(data.data, (c, champion) => {
     $('.championSelect').append(
       $('<option>', {
         value: champion.id,
@@ -166,7 +166,7 @@ function updateChampionSelects(data) {
 }
 
 function updateItemSelects(data) {
-  $.each(data.data, function(i, item) {
+  $.each(data.data, (i, item) => {
     if (item.gold.purchasable && item.maps[11] && item.colloq != '') {
       $('.itemSelect').append(
         $('<option>', {
@@ -179,43 +179,39 @@ function updateItemSelects(data) {
 }
 
 function updateItemStats(data) {
-  $.each(statsCurrentValuesChampion2, function(s, stat) {
-    $('.' + s + ' ' + '.byItems').remove();
-    console.log(document.querySelector('#champion1Stats'));
+  $.each(statsCurrentValuesChampion2, (s, stat) => {
+    $(`.${s} .byItems`).remove();
+    //console.log(document.querySelector('#champion1Stats'));
     statsCurrentValuesChampion1[s] = 0;
     statsCurrentValuesChampion2[s] = 0;
   });
   updateItemStatsChampion1(data);
   updateItemStatsChampion2(data);
-  //console.log(statsCurrentValuesChampion1);
-  //console.log(statsCurrentValuesChampion2);
+  // console.log([statsCurrentValuesChampion1]);
+  // console.log(statsCurrentValuesChampion2);
 }
 
 function updateItemStatsChampion1(data) {
   $(itemsChampion1).each(function() {
-    //console.log(this);
     if (this != '-') {
-      $.each(data.data[this].stats, function(s, stat) {
-        $.each(statsInformation, function(i, item) {
-          $.each(statsInformation[i], function(o, itemStat) {
-            $.each(statsInformation[i][o], function(u, itemStat2) {
+      $.each(data.data[this].stats, (s, stat) => {
+        $.each(statsInformation, (i, item) => {
+          $.each(statsInformation[i], (o, itemStat) => {
+            $.each(statsInformation[i][o], (u, itemStat2) => {
               //asegurarme que hacer si es flat, % o que pedos
               if (s === itemStat2) {
                 //console.log('s === itemStat2', s === itemStat2, s, itemStat2);
-                $('.' + o + ' ' + '.champion1Span').remove();
+                $(`.${o} .champion1Span`).remove();
                 if (s.substring(0, 4) === 'Flat') {
                   statsCurrentValuesChampion1[o] =
                     statsCurrentValuesChampion1[o] + stat;
-                  console.log(o, statsCurrentValuesChampion1[o]);
+                  //console.log(o, statsCurrentValuesChampion1[o]);
                   //console.log(o);
-                  $('#champion1Stats' + ' ' + '.' + o).append(
+                  $(`#champion1Stats .${o}`).append(
                     $('<span>')
                       .attr('class', 'byItems champion1Span')
-                      .append(' + (')
-                      .append(statsCurrentValuesChampion1[o])
-                      .append(')')
+                      .append(` +(${statsCurrentValuesChampion1[o]})`)
                   );
-                  console.log(document.querySelector('#champion1Stats'));
                 }
               }
             });
@@ -228,26 +224,20 @@ function updateItemStatsChampion1(data) {
 
 function updateItemStatsChampion2(data) {
   $(itemsChampion2).each(function() {
-    //console.log(this);
     if (this != '-') {
-      $.each(data.data[this].stats, function(s, stat) {
-        $.each(statsInformation, function(i, item) {
-          $.each(statsInformation[i], function(o, itemStat) {
-            $.each(statsInformation[i][o], function(u, itemStat2) {
-              //asegurarme que hacer si es flat, % o que pedos
+      $.each(data.data[this].stats, (s, stat) => {
+        $.each(statsInformation, (i, item) => {
+          $.each(statsInformation[i], (o, itemStat) => {
+            $.each(statsInformation[i][o], (u, itemStat2) => {
               if (s === itemStat2) {
-                //console.log('s === itemStat2', s === itemStat2, s, itemStat2);
-                $('.' + o + ' ' + '.champion2Span').remove();
-                if (s.substring(0, 4) === 'Flat') {
+                $(`.${o} .champion2Span`).remove();
+                if (s.endsWith('Flat')) {
                   statsCurrentValuesChampion2[o] =
                     statsCurrentValuesChampion2[o] + stat;
-                  //onsole.log(o);
-                  $('#champion2Stats' + ' ' + '.' + o).append(
+                  $(`#champion2Stats .${o}`).append(
                     $('<span>')
                       .attr('class', 'byItems champion2Span')
-                      .append(' + (')
-                      .append(statsCurrentValuesChampion2[o])
-                      .append(')')
+                      .append(`+ (${statsCurrentValuesChampion2[o]})`)
                   );
                 }
               }
@@ -260,59 +250,57 @@ function updateItemStatsChampion2(data) {
 }
 
 function updateStats(data) {
-  var champion1Selected = $('#champion1Select').val();
-  var champion2Selected = $('#champion2Select').val();
-  var levelChampion1 = $('#levelsChampion1Select').val();
-  var levelChampion2 = $('#levelsChampion2Select').val();
+  let champion1Selected = $('#champion1Select').val();
+  let champion2Selected = $('#champion2Select').val();
+  let levelChampion1 = $('#levelsChampion1Select').val();
+  let levelChampion2 = $('#levelsChampion2Select').val();
   $('#champion1Stats').empty();
   $('#champion2Stats').empty();
 
-  $.each(data.data[champion1Selected].stats, function(c, information) {
+  $.each(data.data[champion1Selected].stats, (c, information) => {
     if ($('#levelsChampion1Select').val() == 1) {
       $('#champion1Stats').append(
         $('<li>')
-          .attr('class', 'info list-group-item ' + c)
-          .append(c + ': ')
+          .attr('class', `info list-group-item ${c}`)
+          .append(`${c}: `)
           .append(
             $('<span>')
               .attr('class', 'value')
-              .append(' ' + information)
+              .append(` ${information}`)
           )
       );
     } else {
-      if (c.substr(c.length - 8) !== 'perlevel') {
-        if (data.data[champion1Selected].stats[c + 'perlevel']) {
+      if (!c.endsWith('perlevel')) {
+        if (data.data[champion1Selected].stats[`${c}perlevel`]) {
           $('#champion1Stats').append(
             $('<li>')
-              .attr('class', 'info list-group-item ' + c)
-              .append(c + ': ')
-              .append(
+              .attr('class', `info list-group-item ${c}`)
+              .append(`${c}: `)
+              .append(k
                 $('<span>')
                   .attr('class', 'value')
-                  .append(' ' + information)
+                  .append(` ${information}`)
                   .append(
                     $('<span>')
                       .attr('class', 'perLevel')
-                      .append(' + (')
                       .append(
-                        Number(
-                          data.data[champion1Selected].stats[c + 'perlevel'] *
+                        `+(${Number(
+                          data.data[champion1Selected].stats[`${c}perlevel`] *
                             $('#levelsChampion1Select').val()
-                        ).toFixed(2)
+                        ).toFixed(2)})`
                       )
-                      .append(')')
                   )
               )
           );
         } else {
           $('#champion1Stats').append(
             $('<li>')
-              .attr('class', 'info list-group-item ' + c)
+              .attr('class', `info list-group-item ${c}`)
               .append(c + ': ')
               .append(
                 $('<span>')
                   .attr('class', 'value')
-                  .append(' ' + information)
+                  .append(` ${information}`)
               )
           );
         }
@@ -320,40 +308,38 @@ function updateStats(data) {
       }
     }
   });
-  $.each(data.data[champion2Selected].stats, function(c, information) {
+  $.each(data.data[champion2Selected].stats, (c, information) => {
     if ($('#levelsChampion2Select').val() == 1) {
       $('#champion2Stats').append(
         $('<li>')
-          .attr('class', 'info list-group-item ' + c)
+          .attr('class', `info list-group-item ${c}`)
           .append(c + ': ')
           .append(
             $('<span>')
               .attr('class', 'value')
-              .append(' ' + information)
+              .append(` ${information}`)
           )
       );
     } else {
       if (c.substr(c.length - 8) !== 'perlevel') {
-        if (data.data[champion2Selected].stats[c + 'perlevel']) {
+        if (data.data[champion2Selected].stats[`${c}perlevel`]) {
           $('#champion2Stats').append(
             $('<li>')
               .attr('class', 'info list-group-item ' + c)
-              .append(c + ': ')
+              .append(`${c}: `)
               .append(
                 $('<span>')
                   .attr('class', 'value')
-                  .append(' ' + information)
+                  .append(`' ${information}`)
                   .append(
                     $('<span>')
-                      .attr('class', 'perLevel')
-                      .append(' + (')
+                      .attr('class', `perLevel`)
                       .append(
-                        Number(
-                          data.data[champion2Selected].stats[c + 'perlevel'] *
+                        `+(${Number(
+                          data.data[champion2Selected].stats[`${c}perlevel`] *
                             $('#levelsChampion2Select').val()
-                        ).toFixed(2)
+                        ).toFixed(2)})`
                       )
-                      .append(')')
                   )
               )
           );
@@ -361,23 +347,22 @@ function updateStats(data) {
           $('#champion2Stats').append(
             $('<li>')
               .attr('class', 'info list-group-item ' + c)
-              .append(c + ': ')
+              .append(`${c}:`)
               .append(
                 $('<span>')
                   .attr('class', 'value')
-                  .append(' ' + information)
+                  .append(` ${information}`)
               )
           );
         }
-      } else {
       }
     }
   });
 }
 
 function updatePictures(data) {
-  var champion1Selected = $('#champion1Select').val();
-  var champion2Selected = $('#champion2Select').val();
+  let champion1Selected = $('#champion1Select').val();
+  let champion2Selected = $('#champion2Select').val();
   $('#champion1Picture').attr(
     'src',
     imageURL + data.data[champion1Selected].image.full
@@ -389,29 +374,32 @@ function updatePictures(data) {
 }
 
 function loadImages(data) {
-  $.each(data.data, function(i, item) {
-    if (item.gold.purchasable && item.maps[11] && item.colloq != '') {
-      $.get(itemImageURL + item.image.full)
-        .done(function() {
-          $('#pictures').append(
-            $('<div>')
-              .attr('class', 'row col')
-              .append(
-                $('<img>').attr({
-                  src: itemImageURL + item.image.full,
-                  draggable: true,
-                  ondragstart: 'drag(event)',
-                  id: 'image' + i,
-                  class: 'item '
-                })
-              )
-          );
-        })
-        .fail(function(error) {
-          console.log(error);
-        });
-    }
-  });
+  let items = Object.values(data.data).filter(
+    item =>
+      item.gold.purchasable &&
+      item.maps[11] == true &&
+      item.colloq != '' &&
+      Object.keys(item.stats).length !== 0
+  );
+  items.sort((a, b) => (a.gold.total > b.gold.total ? 1 : -1));
+  for (let item of items) {
+    //console.log(item.image.full.slice(0, 4), item.maps[11]);
+    $.get(itemImageURL + item.image.full)
+      .done(() => {
+        $('#pictures').append(
+          $('<img>').attr({
+            src: itemImageURL + item.image.full,
+            draggable: true,
+            ondragstart: 'drag(event)',
+            id: 'image' + item.image.full.slice(0, 4),
+            class: 'item '
+          })
+        );
+      })
+      .fail(error => {
+        console.log(error);
+      });
+  }
 }
 
 function allowDrop(ev) {
@@ -424,11 +412,11 @@ function drag(ev) {
 
 function drop(ev) {
   ev.preventDefault();
-  var data = ev.dataTransfer.getData('text');
-  var x = document.getElementById(data);
+  let data = ev.dataTransfer.getData('text');
+  let x = document.getElementById(data);
   if (ev.target.tagName === 'DIV') {
     if (ev.target.childNodes.length < 6 || ev.target.id === 'pictures') {
-      x.setAttribute('id', 'copyOf' + data);
+      x.setAttribute('id', `copyOf${data}`);
       ev.target.appendChild(x);
     } else {
       alert('You`ve reached maximum amount of items');
@@ -438,7 +426,7 @@ function drop(ev) {
       ev.target.parentNode.childNodes.length < 6 ||
       ev.target.parentNode.id === 'pictures'
     ) {
-      x.setAttribute('id', 'copyOf' + data);
+      x.setAttribute('id', `copyOf${data}`);
       ev.target.parentNode.appendChild(x);
     } else {
       alert('You`ve reached maximum amount of items');
@@ -446,18 +434,18 @@ function drop(ev) {
   }
   $('#pictures').empty();
   $.getJSON(itemsURL)
-    .done(function(data) {
+    .done(data => {
       loadImages(data);
     })
-    .fail(function(error) {
+    .fail(error => {
       console.log(error);
     });
   getImagesIds();
   $.getJSON(itemsURL)
-    .done(function(data) {
+    .done(data => {
       updateItemStats(data);
     })
-    .fail(function(error) {
+    .fail(error => {
       console.log('Error');
     });
 }
@@ -467,14 +455,13 @@ function getImagesIds() {
   itemsChampion2 = [];
   $.each(
     document.getElementById('itemsChampion1').getElementsByTagName('img'),
-    function(i, item) {
+    (i, item) => {
       itemsChampion1.push(item.id.slice(-4));
     }
   );
-
   $.each(
     document.getElementById('itemsChampion2').getElementsByTagName('img'),
-    function(i, item) {
+    (i, item) => {
       itemsChampion2.push(item.id.slice(-4));
     }
   );
